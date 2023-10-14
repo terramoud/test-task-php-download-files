e8f9729a4120eeff93aeca382c77c16c916b3971
Oleksandr Koreshev <a.koreshev1@gmail.com>
2023-10-14 15:58:44 +0300

    add MySQLConfig.php

---
+++ src/MySQLConfig.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace User\TestTaskPhpDownloadFiles;
+
+class MySQLConfig
+{
+    const DATA_SOURCE = "mysql:host=localhost:3306;dbname=php_test_task_files";
+    const USERNAME = "";
+    const PASSWORD = "";
+}
